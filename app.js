6a462dac9c400744ec5878a542693c2d51f63dbe
Kyle Hawkins <Ksolohawk@gmail.com>
2018-03-28 17:32:26 -0700

    my first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// get the starting height value for each section
+var abtTop = $("#aboutmesection").offset().top;
+var interTop = $('#tutoringsection').offset().top;
+var resTop = $('#resumesection').offset().top;
+var downTop = $('#downloadsection').offset().top;
+//alert([abtBot,interBot,resBot,downBot]);
+
+// have the nav menu track the scrolling
+$(window).on('scroll',function(){
+
+    // we round here to reduce a little workload
+    var current = Math.round($(window).scrollTop());
+
+    //add class to nav item if scroll bar is in each section
+
+    if ((current > 0)&&(interTop > current)) {
+        $("#navAboutMe").addClass("highlight");
+    } else{
+    	$("#navAboutMe").removeClass("highlight");
+    }
+
+    if ((current > interTop)&&(resTop > current)) {
+        $("#navTutoring").addClass("highlight");
+    } else{
+    	$("#navTutoring").removeClass("highlight");
+    }
+
+    if ((current > resTop)&&(downTop > current)) {
+        $("#navResume").addClass("highlight");
+    } else{
+    	$("#navResume").removeClass("highlight");
+    }
+
+    if (current > downTop) {
+        $("#navDownloads").addClass("highlight");
+    } else{
+    	$("#navDownloads").removeClass("highlight");
+    }
+
+});
