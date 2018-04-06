215ad93a12d39069dcc28160fac13a4ab5b6e6ad
Kyle Hawkins <Ksolohawk@gmail.com>
2018-04-06 16:51:43 -0700

    added in image slide shows

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 function btnOpacityOn(){
     document.getElementById("backtotopbtn").style.opacity = ".75";
     document.getElementById("contactinfo").style.opacity = "1.0";
@@ -9,14 +11,89 @@ function btnOpacityOff(){
 }
 
 
+function frameOn(idInput){
+    document.getElementById(idInput).style.border = "3px solid white";
+}
+function frameOff(idInput){
+    document.getElementById(idInput).style.border = "none";
+}
+
+var picMeList = [
+    "me.jpg",
+    "meBackFlip.jpg",
+    "meTree.jpg",
+    "meRome.jpg",
+    "meGraduate.jpg",
+    "meAntCanyon.jpg",
+    "meAfro.jpg"
+];
+var picMeText=[
+    "Me happy!",
+    "Me flipping!",
+    "Me stuck on tree!",
+    "Me in Rome!",
+    "Me surviving!",
+    "Me Arizonaing!",
+    "Me in High School!"
+];
+
+var picMeIndex = 1;
+
+function nextPicAboutMe() {
+
+    var picText = document.getElementById("metext");
+    var pic = document.getElementById("mepic");
+
+    picText.innerHTML = picMeText[picMeIndex];
+    pic.setAttribute("src","./Images/"+picMeList[picMeIndex]);
+
+    picMeIndex++;
+    if(picMeIndex > (picMeList.length-1) ){ picMeIndex=0; }
+    
+
+}
+
+
+
+
+var picIntList = [
+    "meIntSki.png",
+    "meIntBackFlip.jpg",
+    "meIntPaint.jpg",
+    "meIntClimb.jpg"
+];
+var picIntText=[
+    "Cat skiing with my Dad in Steamboat Springs, Co.",
+    "Trampolines were my childhood. This was a bouncy one at a carnival in Italy.",
+    "I like to paint.  When I studied abroad in Itally I took Art:275 (water colors)",
+    "Climbing is one of the best things to do in Tucson.  When it is hot, we climb at a cooler 9,000 ft elevation at the top of Mt. Lemmon"
+];
+
+var picIntIndex = 1;
+
+function nextPicInterests() {
+
+    var picText = document.getElementById("inttext");
+    var pic = document.getElementById("interestpic");
+
+    picText.innerHTML = picIntText[picIntIndex];
+    pic.setAttribute("src","./Images/"+picIntList[picIntIndex]);
+
+    picIntIndex++;
+    if(picIntIndex > (picIntList.length-1) ){ picIntIndex=0; }
+    
+
+}
+
 
 
 
 // get the starting height value for each section
-var abtTop = $("#aboutmesection").offset().top;
-var interTop = $('#tutoringsection').offset().top;
-var resTop = $('#resumesection').offset().top;
-var downTop = $('#downloadsection').offset().top;
+var offSetMove= -200;
+var abtTop = $("#aboutmesection").offset().top + offSetMove;
+var interTop = $('#tutoringsection').offset().top+ offSetMove;
+var resTop = $('#resumesection').offset().top+ offSetMove;
+var downTop = $('#downloadsection').offset().top+ offSetMove;
 //alert([abtBot,interBot,resBot,downBot]);
 
 // have the nav menu track the scrolling
